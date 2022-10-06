9e5443e0d720ba953ab46217f85ec7611568a414
ArjanPeeters <arjan.peeters@gmail.com>
2022-10-06 16:34:18 +0200

    Removed some not used variables

--- HomePage.py
+++ HomePage.py
@@ -34,6 +34,11 @@ def reset():
 
 
 def copy2clipboard():
+
+    naakt = '{naam}_{kenmerk}_{toepassing}'.format(
+        naam=session['naam_gekozen'] if 'naam_gekozen' in session else '',
+        kenmerk=session['kenmerk_gekozen'] if 'kenmerk_gekozen' in session else '',
+        toepassing=session['toepassing_gekozen'] if 'toepassing_gekozen' in session else ''
     st.success('gekopieerd')
 
 
@@ -45,28 +50,21 @@ def main():
         page_title="Naa.K.T. generator",
         page_icon="✍️",
     )
-
     st.image('Logo NAA.K.T.png')
-
-    naakt = '{naam}_{kenmerk}_{toepassing}'.format(
-        naam=session['naam_gekozen'] if 'naam_gekozen' in session else '',
-        kenmerk=session['kenmerk_gekozen'] if 'kenmerk_gekozen' in session else '',
-        toepassing=session['toepassing_gekozen'] if 'toepassing_gekozen' in session else ''
-    )
 
     # build up main selectors in 3 big columns and 2 small columns for the '_'
     reset_col, col1, col2, col3, col4, col5, copy_col = st.columns([1,4, 1, 4, 1, 4,1])
     with col1:
-        st.markdown("""<div style="text-align: right"><h1> {} </h1></div><br>""".format(session['naam_gekozen'] if 'naam_gekozen' in session else 'Naam'), unsafe_allow_html=True)
-        naam = st.selectbox('Naam', options=session['naam'], label_visibility='collapsed', key='naam_gekozen', on_change=callback_naam)
+        st.markdown("""<div style="text-align: right"><h1> {} </h1></div><br>""".format(session['naam_gekozen']if 'naam_gekozen' in session else 'Naam'), unsafe_allow_html=True)
+        naam = st.selectbox('Naam', options=database()['Naam'].unique(), label_visibility='collapsed', key='naam_gekozen', on_change=callback_naam)
 
     with col3:
         st.markdown("""<div style="text-align: center"><h1> {} </h1></div><br>""".format(session['kenmerk_gekozen'] if 'kenmerk_gekozen' in session else 'Kenmerk'), unsafe_allow_html=True)
-        kenmerk = st.selectbox('Kenmerk', options=database().loc[database()['Naam'] == naam, 'Kenmerk'].unique(), label_visibility='collapsed', key='kenmerk_gekozen')
+        st.selectbox('Kenmerk', options=database().loc[database()['Naam'] == naam, 'Kenmerk'].unique(), label_visibility='collapsed', key='kenmerk_gekozen')
 
     with col5:
         st.markdown("""<div style="text-align: left"><h1> {} </h1></div><br>""".format(session['toepassing_gekozen'] if 'toepassing_gekozen' in session else 'Toepassing'), unsafe_allow_html=True)
-        toepassing = st.selectbox('Toepassing', options=database().loc[database()['Naam'] == naam, 'Toepassing'].unique(), label_visibility='collapsed', key='toepassing_gekozen')
+        st.selectbox('Toepassing', options=database().loc[database()['Naam'] == naam, 'Toepassing'].unique(), label_visibility='collapsed', key='toepassing_gekozen')
 
     with col4:
         st.markdown("""<div style="text-align: center"><h1> _ </h1></div><br>""", unsafe_allow_html=True)
