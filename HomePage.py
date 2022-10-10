2b0845f365e4fa4b914fe52fa96c8cb09573e8c1
ArjanPeeters <arjan.peeters@gmail.com>
2022-10-10 13:31:14 +0200

    Made a save option

--- HomePage.py
+++ HomePage.py
@@ -1,17 +1,22 @@
 import streamlit as st
 import pandas as pd
-import pyperclip
 
 session = st.session_state
-# set the lists for Naam Kenmerk en Toepassing in the session state
+
 
 def database():
-
     if 'database' not in session:
         path = 'NAAkt uitgebreid.json'
         session['database'] = pd.read_json(path)
 
     return session['database']
+
+
+def naakt():
+    return '{naam}_{kenmerk}_{toepassing}'.format(
+        naam=session['naam_gekozen'] if 'naam_gekozen' in session else '',
+        kenmerk=session['kenmerk_gekozen'] if 'kenmerk_gekozen' in session else '',
+        toepassing=session['toepassing_gekozen'] if 'toepassing_gekozen' in session else '')
 
 
 def callback_naam():
@@ -30,14 +35,13 @@ def reset():
         session.pop('toepassing_gekozen')
 
 
-def copy2clipboard():
-    naakt = '{naam}_{kenmerk}_{toepassing}'.format(
-        naam=session['naam_gekozen'] if 'naam_gekozen' in session else '',
-        kenmerk=session['kenmerk_gekozen'] if 'kenmerk_gekozen' in session else '',
-        toepassing=session['toepassing_gekozen'] if 'toepassing_gekozen' in session else '')
-    pyperclip.copy(naakt)
-    st.success('gekopieerd naar clipboard: {}'.format(naakt))
+def save():
+    if 'save_list' not in session:
+        session.save_list = []
+    session.save_list.insert(0, naakt())
 
+def reset_list():
+    session.save_list = []
 
 def main():
 
@@ -47,31 +51,45 @@ def main():
         page_title="Naa.K.T. generator"
     )
     st.image('Logo NAA.K.T.png')
+    test = '''
+    <style>
+        .stCode{
+        text-align: center;
+        }
+    </style>
+    '''
+    st.markdown(test, unsafe_allow_html=True)
+    st.code(naakt())
 
     # build up main selectors in 3 big columns and 2 small columns for the '_'
-    reset_col, col1, col2, col3, col4, col5, copy_col = st.columns([2, 8, 1, 8, 1, 8, 2])
+    col_reset, col1, col2, col3, col_save = st.columns([1, 4, 4, 4, 1])
     with col1:
-        st.markdown("""<div style="text-align: right"><h1> {} </h1></div><br>""".format(session['naam_gekozen']if 'naam_gekozen' in session else 'Naam'), unsafe_allow_html=True)
         naam = st.selectbox('Naam', options=database()['Naam'].unique(), label_visibility='collapsed', key='naam_gekozen', on_change=callback_naam)
 
-    with col3:
-        st.markdown("""<div style="text-align: center"><h1> {} </h1></div><br>""".format(session['kenmerk_gekozen'] if 'kenmerk_gekozen' in session else 'Kenmerk'), unsafe_allow_html=True)
+    with col2:
         st.selectbox('Kenmerk', options=database().loc[database()['Naam'] == naam, 'Kenmerk'].unique(), label_visibility='collapsed', key='kenmerk_gekozen')
 
-    with col5:
-        st.markdown("""<div style="text-align: left"><h1> {} </h1></div><br>""".format(session['toepassing_gekozen'] if 'toepassing_gekozen' in session else 'Toepassing'), unsafe_allow_html=True)
+    with col3:
         st.selectbox('Toepassing', options=database().loc[database()['Naam'] == naam, 'Toepassing'].unique(), label_visibility='collapsed', key='toepassing_gekozen')
 
-    with col4:
-        st.markdown("""<div style="text-align: center"><h1> _ </h1></div><br>""", unsafe_allow_html=True)
-    with col2:
-        st.markdown("""<div style="text-align: center"><h1> _ </h1></div><br>""", unsafe_allow_html=True)
-
-    with reset_col:
+    with col_reset:
         st.button('Reset', key='reset_pressed', on_click=reset)
 
-    with copy_col:
-        st.button('Copy', key='copy_pressed', on_click=copy2clipboard)
+    with col_save:
+        st.button('Save', key='save_pressed', on_click=save)
+
+    st.title('')
+    st.markdown('---')
+
+    if 'save_list' in session:
+        left, saved_list, right = st.columns([1, 4, 9])
+        with saved_list:
+            for s in session.save_list:
+                st.code(s)
+        with left:
+            st.button('Empty', key='reset_list_pressed', on_click=reset_list)
+
+    #st.write(session)
 
 
 if __name__ == "__main__":
